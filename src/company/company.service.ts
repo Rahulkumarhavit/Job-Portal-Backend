0e9f330363fb47151d0f8ae21c44e246822476f6
rahulkumarhavit@gmail.com <allblogwork24@gmail.com>
2024-11-25 23:51:32 +0530

    company created api

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { RegisterCompanyDto } from './dto/company.dto';
 
 @Injectable()
-export class CompanyService {}
+export class CompanyService {
+  constructor(private prisma: PrismaService) {}
+
+  async registerCompany(
+    userId: string,
+    registerCompanyDto: RegisterCompanyDto,
+  ) {
+    const { name, description, website, location, logo } = registerCompanyDto;
+    const existingCompany = await this.prisma.company.findUnique({
+      where: { name },
+    });
+    if (!existingCompany) {
+      throw new BadRequestException("you can't add same company.");
+    }
+    const company = this.prisma.company.create({
+      data: {
+        name,
+        description,
+        website,
+        logo,
+        userId,
+      },
+    });
+    return company;
+  }
+}
